c0692bab3c5fc3f482c7395c90ea26990b9254a1
Amritpal Singh <amritpalsinghsahni09@gmail.com>
2025-02-04 11:07:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,10 @@
 int main(){
 int a,b;
 int result;
-char operator;
-scanf("%c",operator);
+char op;
+scanf("%c",op);
 scanf("%d %d %d",&a,&b,&result);
-printf("%d ,%d ,%c,%d\n",a,b,operator,result,a operator b = result);
-result = a operator b;
+printf("%d ,%d ,%c,%d\n",a,b,op,result,a op b = result);
+result = a op b;
 return 0;
 }
