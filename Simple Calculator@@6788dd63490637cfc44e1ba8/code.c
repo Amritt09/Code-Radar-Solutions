f5c448eaa651a6837590d628de93bb92535d32c8
Amritpal Singh <amritpalsinghsahni09@gmail.com>
2025-02-04 11:19:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,7 +6,7 @@ int result;
 char op;
 scanf("%c",&op);
 scanf("%d %d %d",&a,&b,&result);
-printf("%d ,%d ,%c,%d\n",a,b,op,result,a op b = result);
+printf("%d ,%d ,%c,%d\n",a,b,op,result,a op b ,result);
 result = a op b;
 return 0;
 }
