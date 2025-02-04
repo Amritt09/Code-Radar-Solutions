176e7d1bd3bb0ad5c1c781395733ea772c2409bb
Amritpal Singh <amritpalsinghsahni09@gmail.com>
2025-02-04 11:06:21 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a,b;
+int result;
+char operator;
+scanf("%c",operator);
+scanf("%d %d %d",&a,&b,&result);
+printf("%d ,%d ,%c,%d\n",a,b,operator,result,a operator b = result);
+result = a operator b;
+return 0;
+}
