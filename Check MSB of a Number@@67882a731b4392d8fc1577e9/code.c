3a19d9b197320771fbe2a99deb43873dc8a41885
Amritpal Singh <amritpalsinghsahni09@gmail.com>
2025-02-05 23:33:15 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 int findMSB(int num) {
-    if (num == 0) {
-        return -1; // No MSB for 0
+    if (a == 0) {
+        return -1; 
     }
 
     int position = 0;
 
-    // Loop through the bits of the number, starting from the leftmost bit
-    while (num > 0) {
-        num = num >> 1;  // Shift right by one bit
-        position++;  // Move to the next bit
+    while (a> 0) {
+        a = a >> 1;  
+        position++; 
     }
 
-    return position - 1;  // Position of the most significant bit
+    return position - 1;  
 }
 
 int main() {
     int a;
     scanf("%d", &a);  
-    
-n
     int msb_position = findMSB(a);
     
     if (msb_position == -1) {
