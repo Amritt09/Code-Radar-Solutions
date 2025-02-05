98696849016c79efd7f4daf6ddf35ef26ab9fa21
Amritpal Singh <amritpalsinghsahni09@gmail.com>
2025-02-05 10:46:22 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char c;
+    scanf("%c",&c);
+    if ("%c",A){
+        printf("Excellent");
+    }
+    else if("%c",B){
+        printf("Good");
+    }
+    else if("%c",C){printf("Average");}
+    else if ("%c",D){printf("Below Average");}
+    else if ("%c",F){Printf("Fail");}
+    returm 0;
+}
